4d6f86e6ef7c869f041351c435cfc2714a1b81d7
Anton Gridushko <anton.gridushko@gmail.com>
2023-01-18 22:11:26 +0300

    Add decision for a first task.

---
+++ LeetCode/EasyLevel/TwoSumSolution1.cs
@@ -0,0 +1,31 @@
+﻿namespace EasyLevel;
+
+public class TwoSumSolution1
+{
+	public int[] TwoSum(int[] nums, int target)
+	{
+		var returnArray = Array.Empty<int>();
+
+		for (var i = 0; i <= nums.Length; i++)
+		{
+			for (var k = i + 1; k <= nums.Length; k++)
+			{
+				if (k + 1 > nums.Length)
+				{
+					break;
+				}
+				var firstValue = nums[i];
+
+				var secondValue = nums[k];
+
+				if (firstValue + secondValue == target)
+				{
+					returnArray = returnArray.Concat(new[] { i, k }).ToArray();
+
+					break;
+				}
+			}
+		}
+		return returnArray;
+	}
+}
