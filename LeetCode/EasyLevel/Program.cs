c339ef4a91ccbaa7463885bfa2ace5e98e340783
Anton Gridushko <anton.gridushko@gmail.com>
2023-02-19 17:38:25 +0300

    Add 136. Single Number decision. Easy level.

--- LeetCode/EasyLevel/Program.cs
+++ LeetCode/EasyLevel/Program.cs
@@ -1,7 +1,9 @@
 ﻿
 using EasyLevel;
 
-var flowers = new string[] {"flower", "flow", "flight"};
-flowers.FindLongestCommonPrefix();
+//var flowers = new string[] {"flower", "flow", "flight"};
+//flowers.FindLongestCommonPrefix();
+
+Solution.SingleNumber(new []{ 4, 1, 2, 1, 2, 5});
 Console.ReadLine();
 
---
+++ LeetCode/EasyLevel/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace EasyLevel;
+
+public static class Solution
+{
+	public static int SingleNumber(int[] nums)
+	{
+		if (nums == null) return default;
+
+		int counter = 0;
+
+		for (int i = 0; i < nums.Length; i++)
+		{
+			for (int j = 0; j < nums.Length; j++)
+			{
+				var firstElement = nums[i];
+				var secondElement = nums[j];
+				if (firstElement == secondElement)
+				{
+					counter++;
+				}
+			}
+
+			if (counter == 1)
+			{
+				return nums[i];
+			}
+			counter = 0;
+		}
+		return default;
+	}
+}
